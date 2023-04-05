0ee54803f1276293abdbddd184f511d33baa45f0
Regie Marzan <lonebeginner@gmail.com>
2023-04-05 20:47:07 +0800

    add callback js & html & function intro

---
+++ js/arrowFunction.js
@@ -0,0 +1,24 @@
+/*let hi = () => {
+    console.log('Howdy my friend!')
+}
+
+hi();*/
+
+/*let hi = (name) => {console.log(`Howdy ${name} my friend!`)}
+
+hi('regie')*/
+
+/*let add = (a,b) => {return a + b};
+console.log(add(7,3));*/
+
+
+/*let names = ['regie', 'grey', 'syndra', 'soli'].map((name) => {return `howdy ${name}, my friend.`});
+let i=0;
+names.map((name) => {i++;console.log(`howdy ${name}${i}`)});
+console.log(names);*/
+
+
+let names = ['regie', 'grey', 'syndra', 'soli']
+let i=0;
+names.map((name) => {i++;console.log(`howdy ${name} ${i}`)});
+console.log(names);
---
+++ js/callback.js
@@ -0,0 +1,28 @@
+/*let callback = function print(callback){
+    console.log('test');
+}
+
+console.log(typeof callback);*/
+
+// taskOne();
+// taskTwo();
+// taskThree();
+
+function taskOne(){
+    console.log('Task 1')
+}
+
+function taskTwo(){
+    console.log('Task 2')
+}
+
+setTimeout(taskOne, 2000);
+taskTwo();
+
+// function taskThree(){
+//     console.log('Task 3')
+// }
+
+const message = function () {
+    
+}
