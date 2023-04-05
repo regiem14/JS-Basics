9d457300be46f621a4f8f6595dae56ca5894c5ff
Regie Marzan <lonebeginner@gmail.com>
2023-04-05 20:10:28 +0800

    add revealing module example

---
+++ js/built-inNative.js
@@ -0,0 +1,22 @@
+// let pattern = /xyz/;
+// let value = 'This is xyz a test to replace the value of pattern.';
+// console.log(value.replace(pattern, 'hero'));
+// console.log(typeof value);
+// // console.log(typeof pattern);
+
+// let myString = new String('howdy');
+// // console.log(myString);
+// // console.log(myString.toString());
+// console.log(typeof myString);
+
+// let myPrimitive = 'This is PRIMITIVE data type';
+// // console.log(typeof myPrimitive);
+// myPrimitive = myPrimitive.toLowerCase();
+// console.log(myPrimitive);
+
+let myNumber = new Number(14);
+console.log(typeof myNumber);
+
+let myPrimitive2 = myNumber.valueOf();
+console.log(myPrimitive2);
+console.log(typeof myPrimitive2)
--- js/modulePattern.js
+++ js/modulePattern.js
@@ -1,27 +1,27 @@
-var counter = (function(){
+// var counter = (function(){
 
-    let count = 0;
+//     let count = 0;
 
-    function print(message){
-        console.log( message + '---' + count)
-    }
+//     function print(message){
+//         console.log( message + '---' + count)
+//     }
 
-    return{
-        // value: count,
-        get: function(){return count;},
-        set: function(value){count = value;},
-        increment: function(){
-            count += 1;
-            // count = count + 1;
-            print('After Increment')
-        },
-        reset: function(){
-            print('Before reset:');
-            count = 0;
-            print('After reset:');
-        }
-    }
-})();
+//     return{
+//         // value: count,
+//         get: function(){return count;},
+//         set: function(value){count = value;},
+//         increment: function(){
+//             count += 1;
+//             // count = count + 1;
+//             print('After Increment')
+//         },
+//         reset: function(){
+//             print('Before reset:');
+//             count = 0;
+//             print('After reset:');
+//         }
+//     }
+// })();
 
 // console.log(counter.count);
 // console.log(counter.value);
@@ -34,11 +34,45 @@ var counter = (function(){
 // console.log(counter.value);
 
 
-counter.increment();
-counter.increment();
-counter.increment();
-counter.increment();
+// counter.increment();
+// counter.increment();
+// counter.increment();
+// counter.increment();
 
-counter.set(7);
+// counter.set(7);
+// console.log(counter.get());
+// counter.reset();
+
+let counter = (function(){
+    let count = 0;
+
+    function print(message){
+        console.log('$(meassage) => $(count)');
+    }
+
+    function getCount(){ return count;};
+
+    function setCount(value){ count = value};
+    
+    function incrementCount(){
+        count +=1;
+        print('After reset: ');
+    }
+
+    function resetCount(){
+        print('Before reset: ');
+        count= 0;
+        print('After reset: ');
+    }
+
+    return{
+        get: getCount,
+        set: setCount,
+        increment: incrementCount,
+        reset: resetCount
+    }
+})();
+
+console.log(counter);
+console.log(counter.get(8));
 console.log(counter.get());
-counter.reset();
