e796f7504d334417068a51398ba7d912942a0398
Regie Marzan <lonebeginner@gmail.com>
2023-04-06 18:31:06 +0800

    add promise2, promiseAll, callback2 & intro to primse All

---
+++ js/promiseAll.js
@@ -0,0 +1,11 @@
+const promiseOne = new Promise((resolve,reject)=>{
+    resolve("This is Promise One!")
+});
+
+const promeseTwo = new Promise((resolve,reject)=>{
+    resolve("This is Promise Two!")
+});
+
+const promeseThree = new Promise((resolve,reject)=>{
+    resolve("This is Promise Three!")
+});
