a8f7487ab28d2e198fdcf1c97108fc68e00ac412
Regie Marzan <lonebeginner@gmail.com>
2023-04-07 12:11:09 +0800

    add constructor.js

---
+++ js/constructor.js
@@ -0,0 +1,29 @@
+/*let epu = {
+    name: 'rat',
+    division: '1st',
+    batch: 'a'
+}*/
+
+
+/*function Epu(name, division, batch){
+    this.make = name;
+    this.division = division,
+    this.batch = batch
+}
+
+// let epd = new Epu('rat','1st', 14);
+let epd = new Epu('rat','1st');
+
+console.log(epd);*/
+
+
+function MyFunction(){
+    console.log("Regie, is create a simple function.")
+}
+
+let MyFunction = new MyFunction();
+console.log(MyFunction);
+console.log(typeof MyFunction);
+
+console.log(MyFunction);
+console.log(typeof MyFunction)
--- js/promiseAll.js
+++ js/promiseAll.js
@@ -9,3 +9,38 @@ const promeseTwo = new Promise((resolve,reject)=>{
 const promeseThree = new Promise((resolve,reject)=>{
     resolve("This is Promise Three!")
 });
+
+
+/*Promise.all([
+    promiseOne, 
+    promeseTwo, 
+    promeseThree]).then((m)=>
+    (console.log(m)
+));*/
+
+/*Promise.race([
+    promiseOne, 
+    promeseTwo, 
+    promeseThree]).then((m)=>
+    (console.log(m)
+));*/
+
+
+function sleep(time, value, state){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            if(state === "fulfill"){
+                return resolve(value);
+            }else{
+                return reject(new Error(value));
+            }
+        }, time);
+    });
+}
+
+const p1 = sleep(5000, "P1 one", "fulfill");
+const p2 = sleep(1000, "P2 two", "fulfill");
+
+Promise.race([p1,p2]).then((value)=>{
+    console.log(value);
+})
