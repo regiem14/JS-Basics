b36dde8e184b3808634fcecdf8170fbf00dc5e19
Regie Marzan <lonebeginner@gmail.com>
2023-04-07 15:35:36 +0800

    add how to use super() method

--- js/class.js
+++ js/class.js
@@ -1,7 +1,44 @@
-class epu{
+/*class epu{
 
 };
 
 let epu = class {
 
-};
+};*/
+
+
+class epu{
+    constructor(name,division,batch,color){
+        this.name = name;
+        this.division = division;
+        this.batch = batch;
+        this.color = color;
+    }
+    print(){
+        console.log(`${this.name} ${this.division} ${this.batch} ${this.color}`);
+    }
+}
+
+let techsEpu = new epu('lion', '4th', 17);
+console.log(typeof techsEpu);
+// techsEpu.print();
+
+class UnitsDispatch extends epu{
+    constructor(name,division,batch,color){
+        super(name,division,batch);
+        this.color = color;
+    }
+    vanguard(){
+        console.log('They are guard front ' + this.division);
+    }
+
+    show(){
+        console.log(this.color);
+    }
+}
+
+let hqUnitsDispatch = new UnitsDispatch('hellhound', '80th', 23, 'red');
+
+hqUnitsDispatch.print();
+// hqUnitsDispatch.vanguard(); 
+hqUnitsDispatch.show();
